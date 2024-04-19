a6d9f30a6a5dcd39174507ef496c288999455d2a
JuriJuriM <jurimudry@gmail.com>
2024-04-19 20:07:58 +0300

    main page

--- src/components/pages/Main.jsx
+++ src/components/pages/Main.jsx
@@ -1,9 +1,170 @@
 import React from 'react'
 import './Main.scss'
+import Kylie from '../../assets/img/kylie.jpg'
+import First from '../../assets/img/first.jpg'
+import Second from '../../assets/img/second.jpg'
+import Third from '../../assets/img/third.jpg'
+import Fourth from '../../assets/img/fourth.jpg'
+import Cantgen from '../../assets/img/cantget.jpg'
+import Fifth from '../../assets/img/fivth.jpg'
+import Sixth from '../../assets/img/sixth.jpg'
+import Warrior from '../../assets/img/warior.jpg'
+import Seventh from '../../assets/img/seventh.jpg'
+import Eigth from '../../assets/img/eigth.jpg'
 
 const Main = () => {
     return (
-        <div className='main'>Main page</div>
+        <div className='main'>
+            <div className='container'>
+                <div className='main__body'>
+                    <div className='main__title'>Kylie Minogue</div>
+                    <div className='main__kylie'>
+                        <img src={Kylie} alt='Kylie' />
+                    </div>
+                    <div className='main__text'>Kylie Ann Minogue (/mɪˈnoʊɡ/; born 28 May 1968) is an Australian singer, songwriter and actress. Minogue is the highest-selling female recording artist from Australia, having sold over 80 million records worldwide. She has been recognised for reinventing herself in music as well as fashion, and is referred to by the European press as the "Princess of Pop" and a style icon. Her accolades include two Grammy Awards, four Brit Awards and eighteen ARIA Music Awards.</div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={First} alt='First' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>Born and raised in Melbourne, Minogue first achieved recognition starring as Charlene Robinson in the Australian soap opera Neighbours (1986–1988). She began her music career in the late 1980s, releasing four bubblegum and dance-pop-influenced studio albums under PWL.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>By the early 1990s, Minogue had amassed several top ten singles in Australia and the UK, including "The Loco-Motion", "I Should Be So Lucky", "Especially for You", "Hand on Your Heart" and "Better the Devil You Know".</div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Second} alt='Second' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Third} alt='Third' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>Taking more creative control over her music, she signed with Deconstruction Records in 1993 and released the albums Kylie Minogue (1994) and Impossible Princess (1997).</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>By joining Parlophone in 1999, Minogue returned to mainstream dance-oriented music with Light Years (2000), including the number-one hits "Spinning Around" and "On a Night Like This".</div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Fourth} alt='Fourth' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Cantgen} alt='Cantget' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>The follow-up, Fever (2001), was an international breakthrough for Minogue, becoming her best-selling album to date. Its lead single, "Can't Get You Out of My Head" became one of the most successful singles of the 2000s, selling over five million units.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>Follow up singles, "In Your Eyes" and "Love at First Sight" became hits as well. She continued reinventing her image and experimenting with a range of genres on her subsequent albums, which spawned successful singles such as "Slow", "I Believe in You", "2 Hearts" and "All the Lovers".</div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Fifth} alt='Fifth' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Sixth} alt='Sixth' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>In 2017, she partnered with BMG Rights Management, with "Dancing" (2018) as their first release. In the following years, Minogue became the only female artist to have a number-one album and a top ten single, from the 1980s to the 2020s in the UK charts, with Disco (2020) and "Padam Padam" (2023) respectively.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>In film, Minogue made her debut in The Delinquents (1989), and appeared in Street Fighter (1994), Moulin Rouge! (2001), Holy Motors (2012) and San Andreas (2015).</div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Warrior} alt='Warrior' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Seventh} alt='Seventh' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>In reality television, she appeared as a judge on The Voice UK and The Voice Australia both in 2014. Her other ventures include product endorsements, books, perfumes, charitable work and a wine brand.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='main__row'>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__text'>Minogue's achievements include being an ARIA Hall of Fame inductee, Officer of the Order of Australia (AO), Officer of the Order of the British Empire, Chevalier (knight) of the Ordre des Arts et des Lettres and an honorary Doctor of Health Science (D.H.Sc.).</div>
+                            </div>
+                        </div>
+                        <div className='main__column'>
+                            <div className='main__item'>
+                                <div className='main__photo'>
+                                    <img src={Eigth} alt='Eigth' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
