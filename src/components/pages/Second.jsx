f23be282ebde7c4cb18c094184e3ea9612171ac0
JuriJuriM <jurimudry@gmail.com>
2024-04-24 18:13:15 +0300

    second page

--- src/components/pages/Second.jsx
+++ src/components/pages/Second.jsx
@@ -1,9 +1,170 @@
 import React from 'react'
 import './Second.scss'
+import Allkylie from '../../assets/img/allkylie.jpg'
+import Debut from '../../assets/img/debut.jpg'
+import Enjoy from '../../assets/img/enjoy.jpg'
+import Rithmlove from '../../assets/img/rithmlove.jpg'
+import Impossible from '../../assets/img/impossible.jpg'
+import Ligtyears from '../../assets/img/ligtyears.jpg'
+import Ultimate from '../../assets/img/ultimate.jpg'
+import Kissme from '../../assets/img/kissme.jpg'
+import Kyliechristmas from '../../assets/img/kyliechristmas.jpg'
+import Golden from '../../assets/img/golden.jpg'
+import Disco from '../../assets/img/disco.jpg'
 
 const Second = () => {
     return (
-        <div className='second'>Second page</div>
+        <div className='second'>
+            <div className='container'>
+                <div className='second__body'>
+                    <div className='second__title'>Albums discography</div>
+                    <div className='second__allkylie'>
+                        <img src={Allkylie} alt='Allkylie' />
+                    </div>
+                    <div className='second__text'>Australian singer, songwriter and actress Kylie Minogue has released sixteen studio albums, thirteen compilation albums, nine live albums, nine extended plays (EP), thirteen remix albums and two box sets. She is recognised as the highest-selling Australian recording artist of all time by the Australian Recording Industry Association (ARIA), with an estimated career sales of over 80 million units worldwide. Minogue has eight number-one albums on the ARIA Albums Chart, the most for any female Australian artist. In the United Kingdom, she holds the record for being the first female artist to score a number one on the Official Albums Chart in five consecutive decades, from the 1980s to the 2020s.</div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Debut} alt='Debut' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Minogue signed a recording contract with Mushroom Records in early 1987, and released her self-titled debut album the following year. Written and produced by Stock Aitken Waterman, the album was distributed in the UK by Waterman's label, PWL Records. The album spent six weeks at number one in the UK, eventually becoming the fifth highest-selling album of the decade. It has sold over five million copies worldwide.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Her second album, Enjoy Yourself (1989) debuted at number one in the UK and became the sixth best-selling album of the year.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Enjoy} alt='Enjoy' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Rithmlove} alt='Rithmlove' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>She subsequently released Rhythm of Love (1990) and Let's Get to It (1991), both of which reached the top twenty in Australia and the UK. Her final release under PWL Records, Greatest Hits (1992), was her third number-one album in the UK.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>During her years under Deconstruction Records, Minogue released her self-titled album in 1994 and Impossible Princess in 1997. Both albums peaked inside the top ten in Australia and the UK.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Impossible} alt='Impossible' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Ligtyears} alt='Ligtyears' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>After moving to Parlophone in 1999, Minogue earned her first number-one album in her home country with Light Years (2000). She scored the best-selling album of her career with Fever (2001), which sold over six million copies worldwide as of May 2008. It was the thirtieth best-selling album globally in 2002, according to the International Federation of the Phonographic Industry.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Her next releases under Parlophone were studio albums Body Language (2003) and X (2007), as well as the greatest hits album Ultimate Kylie (2004)—all reached platinum status in Australia and the UK. In 2010, Aphrodite debuted at number one in the UK, making her the first female artist to have number-one albums in four consecutive decades.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Ultimate} alt='Ultimate' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Kissme} alt='Kissme' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>In 2012, she released the greatest hits album The Best of Kylie Minogue and the orchestral album The Abbey Road Sessions to celebrate her twenty-fifth year in the music industry. Released during her short-lived management contract with entertainment company Roc Nation, Kiss Me Once (2014) became her fourth number-one album in Australia.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>She collaborated with record producers Fernando Garibay and Giorgio Moroder for two EPs, Sleepwalker (2014) and Kylie and Garibay (2015), before releasing her final record under Parlophone—the Christmas album Kylie Christmas (2015), with a re-release Kylie Christmas: Snow Queen Edition the following year.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Kyliechristmas} alt='Kyliechristmas' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Golden} alt='Golden' />
+                                </div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>In 2017, Minogue signed a global recording contract with BMG Rights Management and a joint deal with Mushroom Music Labels, under the sub-division label Liberator Music for a distribution deal in Australia and New Zealand.</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='second__row'>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__text'>Her subsequent releases under BMG were studio albums Golden (2018), Disco (2020) and Tension (2023), as well as the compilation album Step Back in Time: The Definitive Collection (2019)—all debuted at number one in both Australia and the UK.</div>
+                            </div>
+                        </div>
+                        <div className='second__column'>
+                            <div className='second__item'>
+                                <div className='second__photo'>
+                                    <img src={Disco} alt='Disco' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
